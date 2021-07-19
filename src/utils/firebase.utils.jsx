4cd899bfca1682526329b4de1081f8b60936bd8c
Siddhant Prateek <siddhantprateek@gmail.com>
2021-07-19 23:18:00 +0530

    firebase integration

---
+++ src/utils/firebase.utils.jsx
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAQEwnGlUPDbACj5kHMto3etYDP4UfqGCg",
+    authDomain: "zomato-clone-bb44e.firebaseapp.com",
+    projectId: "zomato-clone-bb44e",
+    storageBucket: "zomato-clone-bb44e.appspot.com",
+    messagingSenderId: "742179519615",
+    appId: "1:742179519615:web:e5a4eac9c86998f89a134a"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
