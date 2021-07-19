c01a3a159fa5589ccd88b4a27e9b5930e0a87ace
Siddhant Prateek <siddhantprateek@gmail.com>
2021-07-19 23:17:30 +0530

    Routing to Zomato App

---
+++ src/Routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom"
+
+
+function Routes() {
+    return(
+        <div className="router">
+            <Switch>
+                <Route path='/'></Route>
+                <Route path='/'></Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default Routes;
+
